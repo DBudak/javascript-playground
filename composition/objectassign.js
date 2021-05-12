f50e2e593d4673ede5ffb2a05a351f25f5be560f
dbudak <dmitriybudak@gmail.com>
2021-05-11 22:48:24 -0500

    spread and o.assign

--- composition/objectassign.js
+++ composition/objectassign.js
@@ -1,2 +1,37 @@
 /*
+
+  Major uses: shallow cloning and combining objects
+
+  INTERESTING DETAIL #1:
+    Assigning a value to existing proprty doesn't define how it behaves.
+    Assignment falls back to defining behavior ONLY when property did not exist.
+    In technical terms it means that for new properties JS uses [DefineOwnProperty] and for existing [Set] from object prototype.
+
+    In practical terms if a proprty has a setter that setter is used when assigning a new value.
 */
+
+// shallow clone example
+const doge = {
+  designation: 'good boi',
+  makeSound: () => {console.log('woof')}
+};
+
+const copyCat = Object.assign({}, doge);
+
+copyCat.makeSound = () => {console.log('meow')};
+
+doge.makeSound(); // woof
+copyCat.makeSound(); // meow
+
+// combination example
+const one = {
+  number: 1
+};
+
+const two = {
+  number: 2,
+  foo: 'bar'
+};
+
+const combined = Object.assign({}, one, two);
+console.log(JSON.stringify(combined)); // {"number":2,"foo":"bar"}
---
+++ composition/spread.js
@@ -0,0 +1,7 @@
+/*
+  Practical uses similar to Object assign
+
+  INTERESTING POINT#1
+    Unlike Object.assign spread yields a new object isntead of assigning to existing
+    Matters only when using Object.assign to create prototype inheritance
+*/
